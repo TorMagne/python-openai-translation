c9dee8368f9b5a6dca6fbfa58c19ae657577446a
TorMagne <tormagne-brekken@outlook.com>
2023-08-23 17:40:42 +0200

    changed errros paths since they where wrong

--- app.py
+++ app.py
@@ -172,10 +172,10 @@ def translation():
 # invalid url
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('/errors/404.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 # internal server error
 @app.errorhandler(500)
 def special_exception_handler(error):
-    return render_template('/errors/500.html'), 500
+    return render_template('errors/500.html'), 500
